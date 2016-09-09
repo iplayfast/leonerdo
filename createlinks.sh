128b8e33abf9878abe3022cdbe77cf5c425227dc
Chris Bruner <cwbruner@gmail.com>
2016-09-09 02:56:01 -0400

    update to use a relative directory instead of direct links

--- createlinks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Run this to create links to OpenCV code that is used here...
-
-OPENCV_ROOT=../opencv
-ln -s ${OPENCV_ROOT}/samples/python/common.py .
-ln -s ${OPENCV_ROOT}/samples/python/video.py .
